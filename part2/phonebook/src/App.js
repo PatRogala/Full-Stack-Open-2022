2151e4590ca8550d111b04344241653b2b52d223
Patryk Rogala <rogala.patryk@pm.me>
2022-12-30 22:39:44 +0100

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,24 @@ import {useState} from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas'}
+        {name: 'Arto Hellas', number: '040-123456'}
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
     const handleNameChange = (event) => {
         setNewName(event.target.value)
     }
 
-    const addName = (event) => {
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
+    const addPerson = (event) => {
         event.preventDefault()
         const nameObject = {
             name: newName,
+            number: newNumber
         }
 
         if (persons.some(person => person.name === newName)) {
@@ -27,16 +33,19 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <form onSubmit={addName}>
+            <form onSubmit={addPerson}>
                 <div>
                     name: <input onChange={handleNameChange} value={newName} />
+                </div>
+                <div>
+                    number: <input onChange={handleNumberChange} value={newNumber} />
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(person => <div key={person.name}>{person.name}</div>)}
+            {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
         </div>
     )
 }
