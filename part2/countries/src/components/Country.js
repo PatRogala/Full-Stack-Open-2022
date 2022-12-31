da83f1468f0cade68ee39908a7e72820542ecbda
Patryk Rogala <rogala.patryk@pm.me>
2022-12-31 12:25:24 +0100

    2.14*: Data for countries, step3

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,4 +1,20 @@
+import axios from "axios";
+import {useState, useEffect} from "react";
+
 const Country = ({country}) => {
+    const weather_api_key = process.env.REACT_APP_WEATHER_KEY
+    const [weather, setWeather] = useState({})
+
+    useEffect(() => {
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.latlng[0]}&lon=${country.latlng[1]}&units=metric&appid=${weather_api_key}`)
+            .then(response => {
+                setWeather(response.data)
+            })
+    }, [])
+
+
+
     return (
         <div>
             <h1>{country.name.common}</h1>
@@ -9,6 +25,12 @@ const Country = ({country}) => {
                 {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
             </ul>
             <img src={country.flags.png} alt="flag" width="100" />
+            {weather.main && <div>
+                <h2>Weather in {country.capital}</h2>
+                <p><b>temperature:</b> {weather.main.temp} Celsius</p>
+                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weather icon" />
+                <p>wind {weather.wind.speed} m/s</p>
+            </div>}
         </div>
     )
 }
