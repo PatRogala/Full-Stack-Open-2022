7524270952cdfd0970bbd2073bbf362b6dbbd233
Patryk Rogala <rogala.patryk@pm.me>
2023-01-08 12:18:36 +0100

    4.5*: helper functions and unit tests, step3

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,22 @@ const dummy = () => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((favorite, blog) => {
+        if (favorite.likes > blog.likes) {
+            return favorite
+        } else {
+            return blog
+        }
+    }, {})
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
