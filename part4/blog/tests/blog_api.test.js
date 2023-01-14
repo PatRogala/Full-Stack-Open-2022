49269aca53ae73da7bce9960c437fd87e9b627d6
Patryk Rogala <rogala.patryk@pm.me>
2023-01-14 20:40:54 +0100

    4.11*: Blog list tests, step4

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -52,6 +52,22 @@ test('a valid blog can be added', async () => {
     expect(blogsAtEnd[2].title).toBe('async/await simplifies making async calls')
 })
 
+test('blog without likes property defaults to 0', async () => {
+    const newBlog = {
+        title: 'async/await simplifies making async calls',
+        author: 'me',
+        url: 'e',
+    }
+
+    await api.post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd[2].likes).toBe(0)
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
