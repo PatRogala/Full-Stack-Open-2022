b3677c7f0a5b07734b064ccbe66b90019d649082
Patryk Rogala <rogala.patryk@pm.me>
2023-01-14 20:27:16 +0100

    4.8: Blog list tests, step1

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    console.log('cleared')
+
+    helper.initialBlogs.forEach(async (blog) => {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+    })
+})
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('there are two blog posts', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(2)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
