da5cd556c245a04a16ad4092319327b567e92fcd
Patryk Rogala <rogala.patryk@pm.me>
2023-02-07 08:58:19 +0100

    1.13*: anecdotes step2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -19,6 +19,13 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  const vote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
 
   const randomAnecdote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
@@ -27,6 +34,8 @@ const App = () => {
   return (
     <div>
       {anecdotes[selected]} <br />
+      has {votes[selected]} votes <br />
+      <Button handleClick={vote} text='vote' />
       <Button handleClick={randomAnecdote} text='next anecdote' />
     </div>
   )
