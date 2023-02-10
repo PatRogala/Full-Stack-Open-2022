348e8e2bb20ad4a4a502ba31e2004cd8db1c97ac
Patryk Rogala <rogala.patryk@pm.me>
2023-02-10 10:05:11 +0100

    Demo app

--- demo/backend/models/note.js
+++ demo/backend/models/note.js
@@ -1,26 +1,15 @@
 const mongoose = require('mongoose')
 
-mongoose.set('useFindAndModify', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
     required: true,
     minlength: 5
   },
-  date: Date,
+  date: {
+    type: Date,
+    required: true,
+  },
   important: Boolean,
 })
 
