ea5c73da84321a0e8456858e7f1e28a7b2430db0
Patryk Rogala <rogala.patryk@pm.me>
2023-02-15 00:31:03 +0100

    4.6*: helper functions and unit tests, step4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -10,13 +10,33 @@ const favoriteBlog = (blogs) => {
   if (blogs.length === 0) {
     return null
   }
-  
+
   const maxLikes = Math.max(...blogs.map(blog => blog.likes))
   return blogs.find(blog => blog.likes === maxLikes)
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const authors_blogs = {}
+  blogs.forEach(blog => {
+    if (authors_blogs[blog.author]) {
+      authors_blogs[blog.author] += 1
+    } else {
+      authors_blogs[blog.author] = 1
+    }
+  })
+
+  const maxBlogs = Math.max(...Object.values(authors_blogs))
+  const author = Object.keys(authors_blogs).find(author => authors_blogs[author] === maxBlogs)
+  return { author, blogs: maxBlogs }
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
