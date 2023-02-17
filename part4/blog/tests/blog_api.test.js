c91848cb8d5422908371d7b29a2ed67823bd5477
Patryk Rogala <rogala.patryk@pm.me>
2023-02-18 00:12:41 +0100

    4.9: Blog list tests, step2

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
---
+++ part4/blog/tests/test_helper.js
@@ -0,0 +1,38 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'HTML is easy',
+    author: "me",
+    url: "http://www.google.com",
+    likes: 5
+  },
+  {
+    title: 'CSS is easy',
+    author: "me",
+    url: "http://www.google.com",
+    likes: 3
+  }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({
+    title: 'CSS is easy',
+    author: "me",
+    url: "http://www.google.com",
+    likes: 3
+  })
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const bloges = await Blog.find({})
+  return bloges.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
