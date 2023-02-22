dda5558cff7098284ab7a44b29c5694add11f713
Patryk Rogala <rogala.patryk@pm.me>
2023-02-22 23:05:28 +0100

    4.21*: bloglist expansion, step9

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -61,6 +61,17 @@ blogsRouter.put('/:id', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog.user.toString() !== decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
