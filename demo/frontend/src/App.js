32bd2b750c80fdb158813839dc00add4b9028026
Patryk Rogala <rogala.patryk@pm.me>
2023-02-24 21:55:32 +0100

    w

--- demo/frontend/src/App.js
+++ demo/frontend/src/App.js
@@ -31,7 +31,6 @@ const App = () => {
     }
   }, [])
 
-
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
