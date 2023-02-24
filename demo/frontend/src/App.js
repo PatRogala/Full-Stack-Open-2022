12b026cb1bf188467e751b1bf839f4caa2073b16
Patryk Rogala <rogala.patryk@pm.me>
2023-02-24 21:55:27 +0100

    e

--- demo/frontend/src/App.js
+++ demo/frontend/src/App.js
@@ -22,6 +22,16 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
+
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
@@ -73,7 +83,9 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
-
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
       noteService.setToken(user.token)
       setUser(user)
       setUsername('')
